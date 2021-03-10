af64e05910f50a5169ba25fd973f82f45359f6a7
SameerBunty <50738759+SAMEERBALANAVAR@users.noreply.github.com>
2021-03-10 12:49:29 +0530

    Initial commit

---
+++ src/components/about.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h2>This is about us page</h2>
+    </div>
+  );
+}
+export default About;
---
+++ src/components/contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <h1>
+      You can find us here:
+      <br />
+      sameer balanavar
+      <br />
+      Kalpita technologies bangalore, <br />
+      Karnataka
+    </h1>
+  );
+}
+
+export default Contact;
